e1afcfad6bbb5a94ff29733d7d3958785bb0cd46
Eяw!и Gontier <ierwinx@me.com>
2022-09-13 18:54:25 -0500

    Initial Commit

---
+++ Pager/PagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PagerApp.swift
+//  Pager
+//
+//  Created by Erwin Luz León on 13/09/22.
+//
+
+import SwiftUI
+
+@main
+struct PagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
